4a3ce67d4eeb6a6fa061721478d209c0677aec45
hydra3333 <hydra3333@gmail.com>
2020-02-21 15:38:02 +1030

    fix a remote authentication issue when updating sources

--- Ubuntu_19.10_xrdp_install_min.sh
+++ Ubuntu_19.10_xrdp_install_min.sh
@@ -76,19 +76,28 @@ if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
 echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
 fi
 
-# Configure the policy xrdp session
+# Configure the policy xrdp session # https://c-nergy.be/blog/?p=14051
 cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
 [Allow Colord all Users]
 Identity=unix-user:*
 Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+#ResultAny=no
+#ResultInactive=no
+#ResultActive=yes
+
+# Configure the policy xrdp session # https://c-nergy.be/blog/?p=14051
+cat > /etc/polkit-1/localauthority/50-local.d/46-allow-update-repo.pkla <<EOF
+[Allow Package Management all Users]
+Identity=unix-user:*
+Action=org.freedesktop.packagekit.system-sources-refresh
 ResultAny=yes
 ResultInactive=yes
 ResultActive=yes
 EOF
-
-#ResultAny=no
-#ResultInactive=no
-#ResultActive=yes
 
 # reconfigure the service
 systemctl daemon-reload
